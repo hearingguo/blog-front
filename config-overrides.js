e03d086ed55ec695d7f16ede0f58c33620b4c5b9
Ivy Guo <ivyguo@vipabc.com>
2018-11-19 18:51:16 +0800

    fix: delete some code

--- config-overrides.js
+++ config-overrides.js
@@ -2,12 +2,6 @@ const rewirePostCss = require('react-app-rewire-postcss');
 const rewireLess = require('react-app-rewire-less');
 const { compose } = require('react-app-rewired');
 
-// module.exports = function override(config, env) {
-//   config = rewireLess(config, env);
-//   config = rewirePostCss(config, env);
-//   return config;
-// }
-
 module.exports = function(config, env) {
   const rewires = compose(
     rewireLess,
