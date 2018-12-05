afaaf93f142de07e02d3b0654008e72531ebbafb
hearingguo <hearingguo@gmail.com>
2018-12-05 08:45:14 +0800

    styles

---
+++ src/config/style.ts
@@ -0,0 +1,28 @@
+const Color = {
+  border: '#eee',
+  theme: '#333',
+  themeGray: '#ccc'
+};
+
+const Size = {
+  s: '12px',
+  m: '14px',
+  l: '18px'
+};
+
+const Gap = {
+  s: '10px',
+  m: '15px',
+  l: '20px'
+};
+
+const Shadow = {
+  blackBottom: '0 5px 10px rgba(0,0,0,0.02)'
+};
+
+export default {
+  Color,
+  Size,
+  Gap,
+  Shadow
+};
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import styles from '../config/style';
+
+const StyleHeader = styled.div`
+  border-bottom: 1px solid ${styles.Color.border};
+  padding: ${styles.Gap.l};
+  box-shadow: ${styles.Shadow.blackBottom};
+`;
 
 class Header extends Component {
   public render() {
-    return <header className="blog-header">策码奔腾 | 目的旅行 | 不务正业</header>;
+    return (
+      <StyleHeader className="blog-header">
+        <img src="../images/logo.png" alt="" />
+        策码奔腾 | 目的旅行 | 不务正业
+      </StyleHeader>
+    );
   }
 }
 
